8fe78e2389df8acd2fbff6d2dd926bb75d5b4353
Ben <brbryant2639@gmail.com>
2021-07-29 17:20:27 -0500

    Initial commit. Setup files with server.js

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const http = require("http")
+const path = require("path")
+
+const app = express()
+
+const server = http.createServer(app);
+
+const { ExpressPeerServer } = require("peer");
+
+const port = process.env.PORT || "8080";
+
+const peerServer = ExpressPeerServer(server, {
+    proxied: true,
+    debug: true,
+    path: "/myapp",
+    ssl: {}
+})
+
+app.use(peerServer);
+
+app.use(express.static(path.join(__dirname)));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+server.listen(port);
+console.log('Listening on: ' + port);
